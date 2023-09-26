6ee545e5ebf861f62a15eaaf4b6efa802216d345
Juli Striglio <juliss2001@hotmail.com>
2023-09-26 14:36:24 -0300

    segundo cambio

---
+++ inicio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from inicio.views import inicio, crear_curso
+
+
+
+urlpatterns = [
+    
+    path('', inicio),
+    path('crear-curso/<str:titulo>/<int:numero>/',crear_curso)
+]
